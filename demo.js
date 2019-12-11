aadd1a920b3944659d43418d2da33afa9ee34bb6
Daykel Muro <daykelms@gmailcom>
2019-12-11 14:37:21 -0500

    package json added

--- demo.js
+++ demo.js
@@ -1,3 +1,10 @@
 var _ = require('lodash')
 
 console.log(_.random(1,100))
+
+var fs = require('fs')
+
+fs.readFile('./data.json', 'utf-8',(err, data) =>{
+    var data = JSON.parse(data)
+    console.log(data.name)
+})
